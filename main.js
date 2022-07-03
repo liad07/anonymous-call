6be2be818af60b758d8a604e3667aab263ef1315
liadk07 <91607695+liad07@users.noreply.github.com>
2022-07-03 22:56:35 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+    function f() {
+        document.getElementById("link").textContent="call here"
+        document.getElementById("link").href="tel:*43"+document.getElementById("tel1").value
+    }
